2bc5bee0c99cd0fda39f78dabf63d63ea58b6519
Mews <ar754456@gmail.com>
2024-02-14 15:12:53 +0000

    Update

--- mousecoords.py
+++ mousecoords.py
@@ -1,6 +1,8 @@
 import pyautogui as pg
 import keyboard as key
+from time import sleep as wait
 
 while True:
     if key.is_pressed("m"):
         print(pg.position())
+        wait(0.5)
--- spawn_location.py
+++ spawn_location.py
@@ -1,4 +1,24 @@
 import pyautogui as pg
+import pyrobloxbot as bot
+from PIL import Image, ImageChops
+import os
+
+SCREENSHOT_REGION = (425, 80, 1175, 360)
+SCREENS_PATH = "spawn_location_screens"
+
+def GETSCREENSHOTS():
+    while True:
+        bot.reset_player()
+        input("Take screenshot...")
+        screen = pg.screenshot(region=SCREENSHOT_REGION)
+        screen.save("spawn_location_screens/"+input("Path:")+".jpg")
 
 def identify():
-    screen = pg.screenshot(region=())
+    screen = pg.screenshot(region=SCREENSHOT_REGION)
+
+    for spawn_location_file in os.listdir(SCREENS_PATH):
+        spawn_location_screen = Image.open(SCREENS_PATH+"/"+spawn_location_file)
+
+        spawn_location_screen.show()
+
+identify()
