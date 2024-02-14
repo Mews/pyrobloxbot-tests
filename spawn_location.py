cea37a893896bd47007eff915910a2cbaf09d976
Mews <ar754456@gmail.com>
2024-02-14 15:32:43 +0000

    Epic identify

--- spawn_location.py
+++ spawn_location.py
@@ -1,7 +1,10 @@
 import pyautogui as pg
 import pyrobloxbot as bot
-from PIL import Image, ImageChops
+import keyboard as key
+from PIL import Image, ImageChops, ImageStat
 import os
+import math
+from time import sleep as wait
 
 SCREENSHOT_REGION = (425, 80, 1175, 360)
 SCREENS_PATH = "spawn_location_screens"
@@ -13,12 +16,40 @@ def GETSCREENSHOTS():
         screen = pg.screenshot(region=SCREENSHOT_REGION)
         screen.save("spawn_location_screens/"+input("Path:")+".jpg")
 
+def get_difference_value(difference_image):
+   #Returns average luminance of difference_image
+   #Lower luminance means less differences
+   im = difference_image.convert('L')
+   stat = ImageStat.Stat(im)
+   return stat.mean[0]
+
+@bot.require_focus
 def identify():
+    wait(0.2) #Give time for window to maximize
+
     screen = pg.screenshot(region=SCREENSHOT_REGION)
 
+    difference_values = []
+
     for spawn_location_file in os.listdir(SCREENS_PATH):
-        spawn_location_screen = Image.open(SCREENS_PATH+"/"+spawn_location_file)
+        path = SCREENS_PATH+"/"+spawn_location_file
+        spawn_location = spawn_location_file.replace(".jpg", "")
+        spawn_location_screen = Image.open(path)
 
-        spawn_location_screen.show()
+        #Generate difference image from screenshot and stored spawn location images
+        difference_image = ImageChops.difference(screen, spawn_location_screen)
 
-identify()
+        difference_values.append( (spawn_location, get_difference_value(difference_image)) )
+    
+    #Return tuple with smallest difference value
+    return min(difference_values, key=lambda tpl: tpl[1])
+
+"""
+#Test if it works
+while True:
+    while not key.is_pressed("y"):
+        wait(0.1)
+    bot.reset_player()
+    wait(5)
+    print(identify())
+"""
