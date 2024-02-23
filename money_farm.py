44480b278f6a75f6edb0e27ba22d4ca7e0b708af
Mews <ar754456@gmail.com>
2024-02-23 21:23:58 +0000

    no way money farm finished lets run

--- money_farm.py
+++ money_farm.py
@@ -1,87 +1,120 @@
 from time import sleep as wait
+from datetime import datetime
 import pyrobloxbot as bot
 import pyautogui as pg
 import pydirectinput as dinput
 from pynput.keyboard import Key, Controller
 
 
-bot.UI_NAV_KEY = "`"
+bot.UI_NAV_KEY = "\\"
 TRIES_TIL_SCROLL = 4
 
-def switch_server():
-    bot.ui_navigate_down()
-    bot.ui_navigate_left()
-    bot.ui_navigate_down()
+@bot.require_focus
+def mega_click(coords, relx=20, rely=0):
+    for i in range(2):
+        dinput.click(coords[0], coords[1])
+        dinput.moveRel(relx, rely)
 
+
+def switch_server():
+    bot.ui_navigate_left()
+
+    #Scroll down
+    kb = Controller()
+    for i in range(50):
+        kb.press(Key.page_down)
+        kb.release(Key.page_down)
+        wait(0.05)
+    
     switched_server = False
 
     tries = 0
 
     while not switched_server:
-        try:
-            bot.ui_click() #Click join button
-            wait(2) #Wait to join
-            ok_button_coords = pg.locateCenterOnScreen("moneyfarm/ok_button.png", confidence=0.9) #Try to find ok button
+        join_button_coords = pg.locateAllOnScreen("moneyfarm/join.jpg", confidence=0.8)
 
-            #Click ok button
-            for i in range(2):
-                dinput.click(ok_button_coords[0], ok_button_coords[1])
-                dinput.moveRel(20, 0)
+        for box in join_button_coords:
+            center = (box.left+(box.width//2), box.top+(box.height//2))
+            mega_click(center)
 
-            tries += 1
+            wait(2)
+            
+            #Check if server was full
+            try:
+                ok_button_coords = pg.locateCenterOnScreen("moneyfarm/ok_button.png", confidence=0.9) #Try to find ok button
 
-            if tries >= TRIES_TIL_SCROLL:
-                tries = 0 #Reset tries
+                #Click ok button
+                for i in range(2):
+                    dinput.click(ok_button_coords[0], ok_button_coords[1])
+                    dinput.moveRel(20, 0)
 
-                #Select scrollable region
-                bot.ui_navigate_up()
+            except pg.ImageNotFoundException:
+                switched_server = True
+                break
+        
+        #Scroll down
+        kb = Controller()
+        for i in range(17):
+            kb.press(Key.page_down)
+            kb.release(Key.page_down)
+            wait(0.1)
 
-                #Scroll down
-                kb = Controller()
-                for i in range(17):
-                    kb.press(Key.page_down)
-                    kb.release(Key.page_down)
-                    wait(0.1)
-
-                bot.ui_navigate_right()
-                bot.ui_navigate_right()
-
-            else:
-                bot.ui_navigate_down()
-
-        except pg.ImageNotFoundException:
-            switched_server = True
 
 
 def grab_chests():
+    #bot.reset_player()
+    #wait(5)
+
     bot.walk_left(5)
-    bot.walk_back(1)
-    bot.walk_right(3)
+    bot.jump(3)
+    bot.walk_left(0.1)
+    bot.walk_back(1.5)
+    bot.walk_right(2)
     bot.jump(2)
     bot.walk_right(0.5)
+    bot.jump(5, 0.1)
+    dinput.keyDown("s")
+    for i in range(3):
+        bot.jump(1)
+        wait(0.3)
+    dinput.keyUp("s")
+    bot.walk_forward(0.2)
+    wait(0.35)
+    bot.walk_back(0.4)
+    bot.walk_left(0.75)
+    bot.walk_right(1.50)
+    bot.walk_back(0.1)
+
 
 
 def wait_for_game_to_load():
-    while True:
-        try:
-            pg.locateOnScreen("moneyfarm/pirates.jpg")
+    try:
+        pg.locateOnScreen("moneyfarm/marine_join.jpg", confidence=0.7)
+        print("Joined new server at", str(datetime.now().time()).split(".")[0])
+        bot.UI_NAV_ENABLED = False
+        return 0
+    except pg.ImageNotFoundException:
+        wait_for_game_to_load()
 
 
 def main():
     while True:
+        wait_for_game_to_load()
+
         #Pick marines
-        bot.ui_navigate_left()
-        bot.ui_click()
-        bot.toggle_ui_navigation()
+        mega_click(pg.locateCenterOnScreen("moneyfarm/marine_join.jpg", confidence=0.7))
+        #bot.toggle_ui_navigation()
+        #bot.ui_navigate_left()
+        #bot.ui_click()
+
+        wait(1.5)
 
         #Open server menu
         bot.ui_click()
         bot.toggle_ui_navigation()
         
-        #grab_chests()
+        grab_chests()
 
-        #switch_server()
-
-        #wait(15) #Wait to join new server
+        switch_server()
 
 main()
