aa7fac7cb5129b414bda4570e1d00b1eeab97137
Mews <ar754456@gmail.com>
2024-02-23 22:32:47 +0000

    ok gl

--- money_farm.py
+++ money_farm.py
@@ -37,7 +37,7 @@ def switch_server():
             #center = (box.left+(round(box.width/2)), box.top+(round(box.height/2)))
             center = (box.left+10, box.top+5)
             mega_click(center)
-            center = (box.left+10, box.top)
+            center = (box.left+10, box.top-5)
             mega_click(center)
 
             wait(2)
